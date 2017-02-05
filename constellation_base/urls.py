32b7e6fc5c22e3075a9593c1e58b33c1888b6a33
Matthew Wynn <matthew@matthewwynn.com>
2017-02-04 21:27:29 -0600

    Add a dashboard page

--- constellation_base/urls.py
+++ constellation_base/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login_view, name='Login'),
-    url(r'^login$', views.login_view),
+    url(r'^$', views.index_view, name='Index'),
+    url(r'^login$', views.login_view, name='Login'),
 ]
--- constellation_base/views.py
+++ constellation_base/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import login, logout
+from django.conf import settings
+from django.utils.module_loading import import_module
+from django.urls import reverse
+
+from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
 from .models import GlobalTemplateSettings, SidebarLink, TitlebarLink
+
+@login_required
+def index_view(request):
+    template_settings_object = GlobalTemplateSettings(allowBackground=True)
+    template_settings = template_settings_object.settings_dict()
+    constellation_apps = []
+    for appname in settings.INSTALLED_APPS:
+        if not "django" in appname:
+            app = import_module(appname)
+            if hasattr(app.views, 'view_dashboard'):
+                constellation_apps.append({'name': appname,
+                                           'url': reverse(app.views.view_dashboard)})
+
+    return render(request, 'constellation_base/index.html', {
+        'template_settings': template_settings,
+        'apps': constellation_apps,
+    })
 
 def login_view(request):
     template_settings_object = GlobalTemplateSettings(allowBackground=True)
