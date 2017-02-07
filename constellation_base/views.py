382b47a5d2c6ae7546b1d0b016c5140b0db97355
Matthew Wynn <matthew@matthewwynn.com>
2017-02-06 22:13:59 -0600

    Fix 'next' parameter in login page

--- constellation_base/views.py
+++ constellation_base/views.py
@@ -31,12 +31,20 @@ def login_view(request):
     template_settings_object = GlobalTemplateSettings(allowBackground=True)
     template_settings = template_settings_object.settings_dict()
     form = LoginForm(request.POST or None)
+    if "next" in request.GET:
+        next_page = request.GET["next"]
+    else:
+        next_page = "/"
     if request.POST and form.is_valid():
         user = form.login(request)
         if user:
             login(request, user)
-            return HttpResponseRedirect("/")
+            if request.POST["next"]:
+                return HttpResponseRedirect(request.POST["next"])
+            else:
+                return HttpResponseRedirect("/")
     return render(request, 'constellation_base/login.html', {
         'form': form,
         'template_settings': template_settings,
+        'next': next_page,
     })
