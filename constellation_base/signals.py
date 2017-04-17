46319aea994aa3078e2980d802f567caa73c031f
Matthew Wynn <matthew@matthewwynn.com>
2017-04-16 23:38:26 -0500

    Add all users into an <All Users> group

--- constellation_base/signals.py
+++ constellation_base/signals.py
@@ -1,6 +1,8 @@
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in
 
+from django.contrib.auth.models import Group
+
 from .models import LocalGroupACLEntry
 
 
@@ -11,3 +13,9 @@ def addSupplementalGroups(sender, user, request, **kwargs):
 
     for group in supGroups.iterator():
         group.group.user_set.add(user)
+
+
+@receiver(user_logged_in)
+def add_to_default_group(sender, user, request, **kwargs):
+    group, _ = Group.objects.get_or_create(name="<All Users>")
+    user.groups.add(group)
